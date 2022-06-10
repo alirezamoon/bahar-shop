a88d979b8ec5e1ddf53540eeeb48af5a91916245
alireza khanamani <khanamani1998@gmail.com>
2022-06-11 00:54:49 +0430

    data

--- data/slider.js
+++ data/slider.js
@@ -1,10 +1,20 @@
 export const sliderData = [
   {
-    title: "شامپو بس",
-    img: "/assets/images/slider1.png",
+    title: "دستبند لاچو",
+    price: "4000000",
+    img: "/assets/images/bracelet.png",
+    count: 10,
   },
   {
-    title: "شامپو داروگر",
-    img: "/assets/images/slider2.png",
+    title: "دستبند نوژین",
+    price: "4000000",
+    img: "/assets/images/bracelet1.png",
+    count: 10,
+  },
+  {
+    title: "دستبند لاو",
+    price: "4000000",
+    img: "/assets/images/bracelet2.png",
+    count: 10,
   },
 ]
